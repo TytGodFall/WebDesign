af4bb7dfe35d16841d4ab96b9681b34b2c381fd1
TytGodFall <150405078+TytGodFall@users.noreply.github.com>
2025-05-06 12:30:17 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int temp = 0;
+
+        Scanner scnr = new Scanner(System.in);
+         System.out.print("Enter a number: ");
+        temp = scnr.nextInt();
+
+        System.out.print("is this your number? : "+temp);
     }
 }
